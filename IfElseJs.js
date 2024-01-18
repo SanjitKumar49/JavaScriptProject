d6766300cfc6cce86689d1157e00f4ea30fa41a1
Sanjit Kumar <kumarsanjit18779@gmail.com>
2024-01-19 02:23:53 +0530

    Basic Conecpt

---
+++ IfElseJs.js
@@ -0,0 +1,25 @@
+let age=1;
+ /* if(age>=18){
+    console.log("you eligible for voting ")
+ }else{
+    console.log("not eligible")
+ }
+*/
+
+ // switch concept
+  /* switch(true){
+    case age>18:
+        console.log("Eligible")
+        break;
+    case age<18:
+        console.log("not Eligible")   
+        break;
+    default:
+        console.log("no data available")    
+ }   
+ */
+
+    // WHile Concept
+   // while(age>18 || age>=18){
+        console.log("while loop")
+  //  }
---
+++ StringJs.js
@@ -0,0 +1,18 @@
+let college="Avit";
+let myName=" Yadav Sanjit Ramesh ";
+console.log(college.length)
+console.log(college.charAt(2))
+console.log(college.indexOf("t"))
+console.log(college.lastIndexOf("r"))
+console.log(myName.trim())
+console.log(college.toUpperCase())
+console.log(college.replaceAll("Avit Chennai"))
+
+// slack() 
+let medName=myName.slice(7, 13);
+let lastName=myName.slice(14);
+console.log(lastName)
+
+// method chaining
+let letter=myName.charAt(4).toUpperCase();
+console.log(letter)
