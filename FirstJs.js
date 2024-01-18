56856edbd1c8f8cec270ad21ec41b93da6745ff0
Sanjit Kumar <kumarsanjit18779@gmail.com>
2024-01-19 00:45:37 +0530

    First JS Program

--- FirstJs.js
+++ FirstJs.js
@@ -2,8 +2,10 @@
    /*print statement program */
 console.log("Hello javaScript");
 console.log("Welcome to Software Enineerig My Jourany ");
+
  /* Single Command */
-// console.log("My name is Sanjit Kumar");
+// console.log("My name is Sanjit Yadav");
+
  /* Multi line command */
   /* console.log("I from Surat ");
   console.log("i passionate to develope Java full stack application and android application"); */
